9daae2cde868094ac351a5259b35458ad6e5ac7b
lendres <lendres@fifthrace.com>
2026-04-23 07:58:22 -0500

    Formatting and comments

--- src/Library/Projects/CreationMethod.cs
+++ src/Library/Projects/CreationMethod.cs
@@ -6,18 +6,14 @@ namespace DigitalProduction.Projects;
 /// Specifies how a project was created.  Allows determination of creation method so behavior can change based on it.
 ///
 /// The "Description" attribute can be accessed using Reflection to get a string representing the enumeration type.
-///
-/// The "Length" enumeration can be used in loops as a convenient way of terminating a loop that does not have to be changed if
-/// the number of items in the enumeration changes.  The "Length" enumeration must be the last item.
-/// for (int i = 0; i &lt; (int)EnumType.Length; i++) {...}
 /// </summary>
 public enum CreationMethod
 {
-	/// <summary></summary>
+	/// <summary>Created by deserializing.</summary>
 	[Description("Deserialized")]
 	Deserialized,
 
-	/// <summary>The number of types/items in the enumeration.</summary>
+	/// <summary>Created by instantiating.</summary>
 	[Description("Instantiated")]
 	Instantiated
 
