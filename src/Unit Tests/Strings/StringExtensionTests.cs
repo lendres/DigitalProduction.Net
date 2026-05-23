59f84b35af909b7b702817b8142d8c53694ce196
Lance A. Endres <lanceendres@gmail.com>
2026-05-23 09:42:01 -0500

    Add unit tests

--- src/Unit Tests/Strings/StringExtensionTests.cs
+++ src/Unit Tests/Strings/StringExtensionTests.cs
@@ -4,6 +4,60 @@ namespace DigitalProduction.UnitTests;
 
 public class StringExtensionsTests
 {
+	#region String Trimming Tests
+
+	#region TrimStart Tests
+
+	[Theory]
+	[InlineData("abcValue", "abc", "Value")]
+	[InlineData("abcabcValue", "abc", "Value")]
+	[InlineData("ValueabcValue", "abc", "ValueabcValue")]
+	[InlineData("Value", "", "Value")]
+	public void TrimStartSingleStringReturnsExpectedResult(string target, string trimString, string expectedResult)
+	{
+		string result = target.TrimStart(trimString);
+		Assert.Equal(expectedResult, result);
+	}
+
+	[Theory]
+	[InlineData("///###Value", "Value")]
+	[InlineData("///Value", "Value")]
+	[InlineData("###Value", "Value")]
+	public void TrimStartMultipleStringsReturnsExpectedResult(string target, string expectedResult)
+	{
+		string result = target.TrimStart("/", "#");
+		Assert.Equal(expectedResult, result);
+	}
+
+	#endregion
+
+	#region TrimEnd Tests
+
+	[Theory]
+	[InlineData("Valueabc", "abc", "Value")]
+	[InlineData("Valueabcabc", "abc", "Value")]
+	[InlineData("ValueabcValue", "abc", "ValueabcValue")]
+	[InlineData("Value", "", "Value")]
+	public void TrimEndSingleStringReturnsExpectedResult(string target, string trimString, string expectedResult)
+	{
+		string result = target.TrimEnd(trimString);
+		Assert.Equal(expectedResult, result);
+	}
+
+	[Theory]
+	[InlineData("Value###///", "Value")]
+	[InlineData("Value///", "Value")]
+	[InlineData("Value###", "Value")]
+	public void TrimEndMultipleStringsReturnsExpectedResult(string target, string expectedResult)
+	{
+		string result = target.TrimEnd("/", "#");
+		Assert.Equal(expectedResult, result);
+	}
+
+	#endregion
+
+	#endregion
+
 	#region Line Ending Comparison Tests
 
 	[Fact]
